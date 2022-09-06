43d63188eb6fd1646ad122e1d70b57949961b935
Farel <farellukas@gmail.com>
2022-09-06 11:41:20 -0600

    Added percent function and clear switching

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,28 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Display from '../../components/Display/Display'
 import Button from '../../components/Button/Button'
 import './App.css'
 
-const BUTTONS = [
-  ['AC', '+/-', '%', '/'],
-  ['7', '8', '9', '*'],
-  ['4', '5', '6', '-'],
-  ['1', '2', '3', '+'],
-  ['0', '.', '=']
-]
-
 function App() {
   const [product, setProduct] = useState("0")
+  const [history, setHistory] = useState([])
+  const [buttons, setButtons] = useState([
+                                  ['AC', '+/-', '%', '/'],
+                                  ['7', '8', '9', '*'],
+                                  ['4', '5', '6', '-'],
+                                  ['1', '2', '3', '+'],
+                                  ['0', '.', '=']
+                                ])
 
   function handleClick(input) {
     if ('1234567890'.includes(input)) {  // numbers
-      product !== "0" ? setProduct(prevProduct => prevProduct + input) : setProduct(input)
-    } else if (input === "AC") {  // clear
+      if (product === "0") {
+        setProduct(input)
+        setButtons(prevButtons => ([
+          ['AC', ...prevButtons[0].slice(1)],
+          ...prevButtons.slice(1)
+        ]))
+      } else {
+        setProduct(prevProduct => prevProduct + input)
+        setButtons(prevButtons => ([
+          ['C', ...prevButtons[0].slice(1)],
+          ...prevButtons.slice(1)
+        ]))
+      }
+    } else if (input === "AC") {  // all clear
+      setProduct("0")
+    } else if (input === "C") { // clear
       setProduct("0")
     } else if (input === "+/-") {  // change sign
       product !== "0" ? product[0] !== "-" ? setProduct(prevProduct => "-" + prevProduct) : setProduct(prevProduct => prevProduct.slice(1)) : setProduct(product)
     } else if (input === '.') {  // decimal
       !product.includes('.') ? setProduct(prevProduct => prevProduct + '.') : setProduct(product)
+    } else if (input === '%') {  // percent
+      product !== "0" ? setProduct(prevProduct => Number(prevProduct/100).toString()) : setProduct(product)
     }
   }
 
@@ -30,12 +46,14 @@ function App() {
     <div className="calc-wrapper">
       <Display value={product} />
       <div className="btn-wrapper">     
-        {BUTTONS.map((row, rowIndex) => {
-          return (<div className="btn-group" key={rowIndex}>
-            {row.map((button, buttonIndex) => {
-              return (<Button id={'btn-' + button} key={buttonIndex} value={button} onClick={handleClick} />)
-            })}
-          </div>)
+        {buttons.map((row, rowIndex) => {
+          return (
+            <div className="btn-group" key={rowIndex}>
+              {row.map((button, buttonIndex) => {
+                return (<Button id={'btn-' + button} key={buttonIndex} value={button} onClick={handleClick} />)
+              })}
+            </div>
+          )
         })}
       </div>
     </div>
